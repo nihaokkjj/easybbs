decfeecd6c441dc61fdf7cb9ac634dc171e78771
nihaokkjj <sai_886@qq.com>
2025-10-18 14:05:23 +0800

    github-action

--- src/utils/Confirm.ts
+++ src/utils/Confirm.ts
@@ -1,6 +1,6 @@
 import { ElMessageBox } from "element-plus";
 
-const Confirm = (message, okfun) => {
+const Confirm = (message: string, okfun: any) => {
   ElMessageBox.confirm(message, '提示', {
     "confirm-buton-text": "确定",
     "cancel_buton-class": "取消",
